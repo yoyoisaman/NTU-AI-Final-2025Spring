9e0ec3513862cfc68e355adf86ca87c073f033d5
yijia <yijia07080@gmail.com>
2025-06-02 01:42:21 +0800

    add visualization of learn results

--- visualize/decompose.py
+++ visualize/decompose.py
@@ -66,24 +66,16 @@ obj2_data2 = [
     ("wooden pickaxe", 15),
 ]
 
-obj3_data1 = obj1_data1
-obj3_data2 = obj1_data2
-obj4_data1 = obj2_data1
-obj4_data2 = obj2_data2
-
 sns.set_style("whitegrid", {"grid.color": "#F2F2F2"})
-
-fig, axes = plt.subplots(2, 2, figsize=(10.6, 3.7 * 2 + 0.5))
-plot_craft_data(axes[0, 0], obj1_data1, obj1_data2, "Craft a Golden Sword")
-plot_craft_data(axes[0, 1], obj2_data1, obj2_data2, "Collect a Lava Bucket")
-plot_craft_data(axes[1, 0], obj3_data1, obj3_data2, "Third Scenario")
-plot_craft_data(axes[1, 1], obj4_data1, obj4_data2, "Fourth Scenario")
+fig, axes = plt.subplots(1, 2, figsize=(10.6, 3.7 + 0.5))
+plot_craft_data(axes[0], obj1_data1, obj1_data2, "Craft a Golden Sword")
+plot_craft_data(axes[1], obj2_data1, obj2_data2, "Collect a Lava Bucket")
 
 orange_line = Line2D([0], [0], color="orange", linewidth=2, solid_capstyle="butt")
 purple_line = Line2D([0], [0], color="purple", linewidth=2, solid_capstyle="butt")
 fig.legend(
     handles=[orange_line, purple_line],
-    labels=["Voyager+ (Ours)", "Voyager"],
+    labels=["Voyager+++ (Ours)", "Voyager"],
     loc="lower center",
     ncol=2,
     frameon=False,
@@ -91,9 +83,10 @@ fig.legend(
     columnspacing=0.7
 )
 
-line = Line2D([0, 1], [0.045, 0.045], transform=fig.transFigure, color="#C8C8C8", linewidth=0.5, solid_capstyle="butt")
+line = Line2D([0, 1], [0.085, 0.085], transform=fig.transFigure, color="#C8C8C8", linewidth=0.5, solid_capstyle="butt")
 fig.add_artist(line)
 
-plt.tight_layout(rect=[0, 0.053, 1, 1])
-fig.savefig("./visualize/output/decompose_4obj.svg", dpi=300)
+plt.tight_layout(rect=[0, 0.1, 1, 1])
+plt.savefig("./visualize/output/decompose.svg", dpi=300)
+plt.savefig("./visualize/output/decompose.png", dpi=300)
 plt.show()
